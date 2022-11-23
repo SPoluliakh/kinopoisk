8fa0331cf408636905838ba923ad23752b9a31ca
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2022-11-23 16:32:02 +0200

    add listOfMoves

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 const makeGenre = (genres = []) => {
-  return genres.map(genre => `<li class="movie-card_genre-item">${genre}</li>`);
-};
+  let result = '';
+  genres.map((genre,index) => {
+    if (index !== genres.length-1){result += `<li class="movie-card_genre-item">${genre.toString()[0].toUpperCase() + genre.toString().slice(1)}, </li>`}
+    else {result += `<li class="movie-card_genre-item">${genre.toString()[0].toUpperCase() + genre.toString().slice(1)} </li>`}})
+  return result
+  };
 
 const makeMovieCard = (results = []) => {
-  return results.map(
-    ({ poster_path, title, genre_ids }) => `<li class="movie-card">
+  let result = '';
+  results.map(
+    ({ poster_path, title, genre_ids }) => result +=`<li class="movie-card"><div>
   <img class="movie-card_img" src="https://image.tmdb.org/t/p/original/${poster_path}" alt="${title}" />
   <h2 class="movie-card_title">${title}</h2>
-  <ul class="movie-card_genre-list">${makeGenre(genre_ids)}</ul>
-</li>
-`
-  );
+  <ul class="movie-card_genre-list">${makeGenre(genre_ids)}</ul></div>
+</li>`
+  )
+  return `<div class="movie-card__container">${result}/div>`
 };
 
 const API_KEY = `21ae8667e2d219d1bbc4eb64edced0b5`;
@@ -25,4 +30,5 @@ fetch(url)
     console.log(data);
     const a = makeMovieCard(data.results);
     console.log(a);
+    document.querySelector('.movie-card').insertAdjacentHTML("afterbegin",a)
   });
