f5753b4976f369dc645fa44d1f589999738dd796
SPoluliakh <S_Poluliakh@ukr.net>
2022-11-26 21:55:11 +0200

    added close modal functions

--- src/js/functions/openMovieInfo.js
+++ src/js/functions/openMovieInfo.js
@@ -13,15 +13,40 @@ import {
 export async function openMovieInfo(evt) {
   const film = evt.target;
   if (!film.classList.contains('movie-card')) return;
+
   const filmData = await getMovieById(film.dataset.id);
   const { poster_path, title, genres, vote_count, vote_average, popularity } =
     filmData.data;
+
   const card = makeModalCard(title, vote_count, vote_average, popularity);
   modalRef.innerHTML = card;
   modalImgRef.src = `https://image.tmdb.org/t/p/original/${poster_path}`;
   modalImgRef.alt = `${title}`;
   modalTitleRef.textContent = `${title}`;
-  const a = makeModalGenre(genres);
-  modalGenresRef.textContent = `${a}`;
+  const genreList = makeModalGenre(genres);
+  modalGenresRef.textContent = `${genreList}`;
+
   backdropRef.classList.remove('is-hidden');
+  document.body.style.overflow = 'hidden';
+  window.addEventListener('keydown', closeByEsc);
+  backdropRef.addEventListener('click', closeByClick);
+}
+
+function closeByClick(evt) {
+  if (evt.target.classList.contains('backdrop')) {
+    closeModal();
+  }
+}
+
+function closeByEsc(evt) {
+  if (evt.code === 'Escape') {
+    closeModal();
+  }
+}
+
+function closeModal() {
+  backdropRef.classList.add('is-hidden');
+  document.body.style.overflow = '';
+  window.removeEventListener('keydown', closeByEsc);
+  backdropRef.removeEventListener('click', closeByClick);
 }
