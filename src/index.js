60063611f8ac6cda85c5864ebb79e96d549a1f6f
SPoluliakh <S_Poluliakh@ukr.net>
2022-11-28 14:47:48 +0200

    fixed

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import { moviesListMarkupFirstRender } from './js/functions/render-home-page';
 import { setGenreOptions } from './js/functions/local-storage';
 import { openMovieInfo } from './js/functions/openMovieInfo';
 import { listRef } from './js/refs/refs';
-import spinner from './js/components/spinner';
+
 import './js/functions/searchFilm';
 
 document.addEventListener('DOMContentLoaded', () => {
   setGenreOptions();
   moviesListMarkupFirstRender();
   listRef.addEventListener('click', openMovieInfo);
-  spinner();
 });
