28909e810088a2f067218df1a3bf88a10c700eef
AfanasevKirillGitHub <afanasev.dp@gmail.com>
2022-11-29 23:12:52 +0200

    kids-page

--- src/js/functions/pagination.js
+++ src/js/functions/pagination.js
@@ -3,30 +3,32 @@ import 'tui-pagination/dist/tui-pagination.css';
 import { moviesListMarkupFirstRender } from '../functions/render-home-page';
 
 const container = document.getElementById('tui-pagination-container');
-const options = { // below default value of options
-    totalItems: 10000,
-    itemsPerPage: 20,
-    visiblePages: 5,
-    page: 1,
-    centerAlign: true,
-    firstItemClassName: 'tui-first-child',
-    lastItemClassName: 'tui-last-child',
-    template: {
-        page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-        currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-        moveButton:
-            '<a href="#" class="tui-page-btn tui-{{type}}">' +
-                '<span class="tui-ico-{{type}}">{{type}}</span>' +
-            '</a>',
-        disabledMoveButton:
-            '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-                '<span class="tui-ico-{{type}}">{{type}}</span>' +
-            '</span>',
-        moreButton:
-            '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-                '<span class="tui-ico-ellip">...</span>' +
-            '</a>'
-    }
+const options = {
+  // below default value of options
+  totalItems: 10000,
+  itemsPerPage: 20,
+  visiblePages: 5,
+  page: 1,
+  centerAlign: true,
+  firstItemClassName: 'tui-first-child',
+  lastItemClassName: 'tui-last-child',
+  template: {
+    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    currentPage:
+      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    moveButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</span>',
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+      '<span class="tui-ico-ellip">...</span>' +
+      '</a>',
+  },
 };
 
 const pagination = new Pagination(container, options);
@@ -36,8 +38,8 @@ const pagination = new Pagination(container, options);
 pagination.on('afterMove', updatePage);
 
 async function updatePage(event) {
-    const currentPage = event.page;
-    console.log('currentPage', currentPage);
+  const currentPage = event.page;
+  console.log('currentPage', currentPage);
 
-    await moviesListMarkupFirstRender(currentPage);
+  await moviesListMarkupFirstRender(currentPage);
 }
