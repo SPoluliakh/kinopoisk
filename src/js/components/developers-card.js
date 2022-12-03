3603038279b6520acd669dd1c8dc92946527908b
SPoluliakh <S_Poluliakh@ukr.net>
2022-12-03 14:14:13 +0200

    fixed foters modal

--- src/js/components/developers-card.js
+++ src/js/components/developers-card.js
@@ -1,12 +1,11 @@
 import Data from '../../../developers.json';
-
 import { linkedinRef, githubRef } from '../refs/refs';
 
 const developers = Data.developers;
 
 export function appendDevelopersMarkup() {
-  const github = './sprite.f14d31f7.svg#icon-github';
-  const linkedIn = './sprite.f14d31f7.svg#icon-linkedin';
+  let github = githubRef.getAttribute('href');
+  let linkedIn = linkedinRef.getAttribute('href');
 
   return developers
     .map(({ name, position, git, photo300, photo600, linkedin }) => {
@@ -18,17 +17,16 @@ export function appendDevelopersMarkup() {
         <ul class="dev-social-network">
             <li class="dev-social-network__item">
                 <a class="dev-social-network__link" href="${git}" target='_blank'>
-                 <svg class="dev-social-network__icon icon-github">
-                    <use href="${github}"></use>
+                 <svg class="dev-social-network__icon ">
+                    <use class="icon-github" href=${github}></use>
                </svg>
                 </a>
             </li>
             <li class="dev-social-network__item">
                 <a class="dev-social-network__link" href="${linkedin}" target='_blank'>
-              <svg
-              class="dev-social-network__icon icon-linkedin">                  
-                <use href="${linkedIn}"></use>
-                  </svg>
+              <svg class="dev-social-network__icon" >
+               <use class="icon-linkedin" href=${linkedIn}></use>              
+              </svg>
                 </a>
             </li>
         </ul>
