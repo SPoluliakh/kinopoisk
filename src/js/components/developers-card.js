73197395940953912b2f16a145c1f94d13c240fd
SPoluliakh <S_Poluliakh@ukr.net>
2022-12-03 13:00:23 +0200

    checked

--- src/js/components/developers-card.js
+++ src/js/components/developers-card.js
@@ -1,31 +1,12 @@
-const developers = [
-  {
-    name: 'Sergii Poluliakh',
-    position: 'Team lead',
-    git: 'https://github.com/SPoluliakh',
-    linkedin: 'https://www.linkedin.com/in/sergii-poluliakh/',
-    photo300: 'https://i.ibb.co/L0WX694/300.jpg',
-    photo600: 'https://i.ibb.co/42myh6n/600.jpg',
-  },
-  {
-    name: 'Vlad Popov',
-    position: 'Scrum master',
-    git: 'https://github.com/StudentVlad5',
-    linkedin: 'https://www.linkedin.com/in/vladyslav-popov-a491232a/',
-    photo300: 'https://i.ibb.co/0Jfz9h0/image.jpg',
-    photo600: 'https://i.ibb.co/0Jfz9h0/image.jpg',
-  },
-];
-
 import Data from '../../../developers.json';
 
 import { linkedinRef, githubRef } from '../refs/refs';
 
-// const developers = Data.developers;
+const developers = Data.developers;
 
 export function appendDevelopersMarkup() {
-  const github = '/sprite.f14d31f7.svg#icon-github';
-  const linkedIn = '/sprite.f14d31f7.svg#icon-linkedin';
+  const github = './sprite.f14d31f7.svg#icon-github';
+  const linkedIn = './sprite.f14d31f7.svg#icon-linkedin';
 
   return developers
     .map(({ name, position, git, photo300, photo600, linkedin }) => {
