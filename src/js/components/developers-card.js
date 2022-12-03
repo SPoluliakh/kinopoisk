8bb47d1c81897b22ba8e9053b6a82afe0b4def3c
SPoluliakh <S_Poluliakh@ukr.net>
2022-12-03 12:56:43 +0200

    checked

--- src/js/components/developers-card.js
+++ src/js/components/developers-card.js
@@ -1,17 +1,35 @@
+const developers = [
+  {
+    name: 'Sergii Poluliakh',
+    position: 'Team lead',
+    git: 'https://github.com/SPoluliakh',
+    linkedin: 'https://www.linkedin.com/in/sergii-poluliakh/',
+    photo300: 'https://i.ibb.co/L0WX694/300.jpg',
+    photo600: 'https://i.ibb.co/42myh6n/600.jpg',
+  },
+  {
+    name: 'Vlad Popov',
+    position: 'Scrum master',
+    git: 'https://github.com/StudentVlad5',
+    linkedin: 'https://www.linkedin.com/in/vladyslav-popov-a491232a/',
+    photo300: 'https://i.ibb.co/0Jfz9h0/image.jpg',
+    photo600: 'https://i.ibb.co/0Jfz9h0/image.jpg',
+  },
+];
+
 import Data from '../../../developers.json';
 
 import { linkedinRef, githubRef } from '../refs/refs';
 
-const developers = Data.developers;
+// const developers = Data.developers;
 
 export function appendDevelopersMarkup() {
   const github = '/sprite.f14d31f7.svg#icon-github';
   const linkedIn = '/sprite.f14d31f7.svg#icon-linkedin';
 
-  try {
-    return developers
-      .map(({ name, position, git, photo300, photo600, linkedin }) => {
-        return `<li class="dev-modal__card">
+  return developers
+    .map(({ name, position, git, photo300, photo600, linkedin }) => {
+      return `<li class="dev-modal__card">
         <img class="dev-modal__picture" src="${photo300}" alt="${name} >
     <div class="dev-modal__wrapper">
         <h2 class="dev-modal__name">${name}</h2>
@@ -35,9 +53,6 @@ export function appendDevelopersMarkup() {
         </ul>
     </div>
 </li>`;
-      })
-      .join('');
-  } catch (err) {
-    console.log(err);
-  }
+    })
+    .join('');
 }
